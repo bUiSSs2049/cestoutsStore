29fc2af6e1fecaa9566715379345afa58e825cc6
aissa <89082595+bUiSSs2049@users.noreply.github.com>
2023-04-21 15:02:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,6 @@
 var express = require('express');
-var session = require('express-session');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
+
 var app = express();
-
-var passport = require('passport');
-var flash = require('connect-flash');
-
-require('./config/passport')(passport);
-
-app.use(morgan('dev'));
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({
- extended: true
-}));
-
-app.set('view engine', 'ejs');
-
-app.use(session({
- secret: 'justasecret',
- resave:true,
- saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-
-
-
 
 
 
@@ -40,3 +11,5 @@ app.use(flash());
 app.get('/', (req, res) => {
     res.send('t')
 })
+
+app.listen(Process.env.PORT);
