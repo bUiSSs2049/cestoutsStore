9acc9312463b056667e3224df549946520121f08
aissa <89082595+bUiSSs2049@users.noreply.github.com>
2023-04-20 23:35:55 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 var app = express();
 
+var passport = require('passport');
+var flash = require('connect-flash');
+
+require('./config/passport')(passport);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+ extended: true
+}));
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+ secret: 'justasecret',
+ resave:true,
+ saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+
+
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
-        res.send('rr')
-
-
-    });
-
-
-  app.listen(process.env.PORT || 3000)
+    res.send('t')
+})
